4ffab2009467b474d00b4cb10e43f2777fec4b48
Leandro Ostera <leandro@ostera.io>
2015-04-26 17:33:24 -0300

    Go back to just logging

--- lib/switchboard.rb
+++ lib/switchboard.rb
@@ -120,17 +120,7 @@ module Switchboard
       type = payload[0]
       body = payload[1]
 
-      preview = Hash[body].map do |k, v|
-        if k == "list"
-          v.map do |m|
-            m["raw"] = "OMITTED" if m["raw"]
-            m
-          end
-        else
-          v
-        end
-      end
-      p [:dispatch, type, preview]
+      p [:dispatch, type, body]
 
       case type 
       when 'newMessage'
